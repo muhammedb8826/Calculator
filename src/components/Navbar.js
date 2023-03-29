5e1073c0b4aa1829d5aceb3ab69fdd1b57799d3b
Muhammed Berisso <muhammedb8826@gmail.com>
2023-03-30 02:52:58 +0300

    Add active class to the link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-title">
+      <NavLink to="/" className="site-title">
         Math Magicians
-      </Link>
+      </NavLink>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/calculator">Calculator</Link>
+          <NavLink to="/calculator">Calculator</NavLink>
         </li>
         <li>
-          <Link to="/quote">Quote</Link>
+          <NavLink to="/quote">Quote</NavLink>
         </li>
       </ul>
     </nav>
